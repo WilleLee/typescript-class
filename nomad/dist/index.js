97931912faecc3858ec91cc0d710e00bddf6f927
Wille Lee <1992season@gmail.com>
2022-04-02 17:09:29 +0900

    ts classes added

--- nomad/dist/index.js
+++ nomad/dist/index.js
@@ -1,7 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello, ${name}, you are ${age}, you are ${gender}!`;
-};
-console.log(sayHi("Wille", 13, "male"));
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+const genesisBlock = new Block(0, "235246", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- nomad/src/index.ts
+++ nomad/src/index.ts
@@ -1,7 +1,44 @@
-const sayHi = (name:string, age:number, gender:string):string /*what i am going to return? void? string? something else?*/ => {
-    return `Hello, ${name}, you are ${age}, you are ${gender}!`;
-};
+import * as CryptoJS from "crypto-js";
 
-console.log(sayHi("Wille", 13, "male"));
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockhash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+const genesisBlock:Block = new Block(0, "235246", "", "Hello", 123456);
+
+let blockchain: Block[] = [genesisBlock];
+
+console.log(blockchain);
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
